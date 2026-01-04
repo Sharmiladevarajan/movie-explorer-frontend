2fb61365580a381f58b921e2f7a9d1fc0d1082c4
Sharmila Devarajan <68813975+Sharmiladevarajan@users.noreply.github.com>
2026-01-04 11:45:10 +0530

    Add image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Force new build
+  images: {
+    domains: ['m.media-amazon.com', 'image.tmdb.org'],
+  },
 }
 
 module.exports = nextConfig
