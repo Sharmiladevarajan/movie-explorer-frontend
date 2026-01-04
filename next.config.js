7fc70a6eb0bf46becb10d79fa964ca8611dab2c8
Sharmila Devarajan <68813975+Sharmiladevarajan@users.noreply.github.com>
2026-01-04 11:51:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['m.media-amazon.com', 'image.tmdb.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+    ],
   },
 }
 
